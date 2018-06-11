d8889d955a715f2f9c6330dac2fd988154eaa035
mayur.m.patil <mayur.m.patil@WMSI003883>
2018-06-11 12:17:37 +0200

    components

--- WebDemo/WebDemo/Angular-Demo/src/app/app.module.ts
+++ WebDemo/WebDemo/Angular-Demo/src/app/app.module.ts
@@ -3,11 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material';
+
+import { MatInputModule } from '@angular/material';
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserloginComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
